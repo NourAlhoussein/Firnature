dad754faf629fd95aa16754625680199f09fc605
nour alhoussein <nour.alhoussein@axtro-soft.com>
2024-10-03 12:31:47 +0300

    modify about the small nav to close nav when i click any button

--- js.js
+++ js.js
@@ -1,9 +1,13 @@
 // JavaScript for adding/removing CSS class on scroll
-window.addEventListener('scroll', function () {
-    var navbar = document.getElementById('navbar');
-    if (window.pageYOffset > 0) {
-        navbar.classList.add('navbar-scrolled');
-    } else {
-        navbar.classList.remove('navbar-scrolled');
-    }
+window.addEventListener("scroll", function () {
+  var navbar = document.getElementById("navbar");
+  if (window.pageYOffset > 0) {
+    navbar.classList.add("navbar-scrolled");
+  } else {
+    navbar.classList.remove("navbar-scrolled");
+  }
 });
+
+function removeSmallNav() {
+  document.getElementById('close_button').click();
+}
